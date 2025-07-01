cb41470f304c003c56cc638715f6955ae54a7427
MohamedMoustakim <mohamedmoustakim77@gmail.com>
2025-07-01 16:51:45 +0100

    bloden

--- src/pegas/Blog.jsx
+++ src/pegas/Blog.jsx
@@ -1,47 +1,47 @@
-// import React from 'react';
-// import caroul3 from '../assets/images-44/carousel3.webp'
-// import imageOure from "../assets/images-44/blog-01.jpg";
-// import imageOure1 from "../assets/images-44/blog-02.jpg";
+import React from 'react';
+import caroul3 from '../assets/images-44/carousel3.webp'
+import imageOure from "../assets/images-44/blog-01.jpg";
+import imageOure1 from "../assets/images-44/blog-02.jpg";
 
-// const Blog = () => {
-//     return (
-//         <section>
-//             <div className="relative w-full">
-//                 <img
-//                     src={caroul3}
-//                     className="w-full h-60 object-cover"
-//                 />
-//                 <h1 className="absolute inset-0 flex items-center justify-center text-white text-6xl font-bold">
-//                     NEWS
-//                 </h1>
-//             </div>
-//             <div className='flex flex-col gap-10 px-10 pt-10'>
-//                 <div className="flex flex-col w-full md:w-1/3">
-//                     <img
-//                         src={imageOure}
-//                         alt=""
-//                         className="w-full h-60 object-cover transition-transform duration-300 hover:scale-105 hover:shadow-xl"
-//                     />
-//                     <h1 className="text-gray-700 text-lg font-semibold mt-4 hover:text-red-400">Black Friday Guide: Best Sales & Discount <br />Codes</h1>
-//                     <p className="text-sm text-gray-500 mt-1 pt-2">by fashe-theme Admin on Dec 28, 2017</p>
-//                     <p className="text-sm text-gray-400 mt-2 pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Etiam sed turpis sed lorem dignissim vulputate nec <br />cursus ante. Nunc sit...</p>
-//                 </div>
-//                 <div className="w-full md:w-1/3">
-//                     <img
-//                         src={imageOure1}
-//                         alt=""
-//                         className="w-full h-60 object-cover transition-transform duration-300 hover:scale-105 hover:shadow-xl"
-//                     />
-//                     <h1 className="text-gray-700 text-lg font-semibold mt-4 hover:text-red-400">The White Sneakers Nearly Every Fashion <br />Girls Own</h1>
-//                     <p className="text-sm text-gray-500 mt-1 pt-2">by fashe-theme Admin on Dec 28, 2017</p>
-//                     <p className="text-sm text-gray-400 mt-2 pt-3">Duis ut velit gravida nibh bibendum commodo. Suspendisse <br />pellentesque mattis augue id euismod. <br />Interdum et...</p>
-//                 </div>
-//             </div>
-//             <div></div>
+const Blog = () => {
+    return (
+        <section>
+            <div className="relative w-full">
+                <img
+                    src={caroul3}
+                    className="w-full h-60 object-cover"
+                />
+                <h1 className="absolute inset-0 flex items-center justify-center text-white text-6xl font-bold">
+                    NEWS
+                </h1>
+            </div>
+            <div className='flex flex-col gap-10 px-10 pt-10'>
+                <div className="flex flex-col w-full md:w-1/3">
+                    <img
+                        src={imageOure}
+                        alt=""
+                        className="w-full h-60 object-cover transition-transform duration-300 hover:scale-105 hover:shadow-xl"
+                    />
+                    <h1 className="text-gray-700 text-lg font-semibold mt-4 hover:text-red-400">Black Friday Guide: Best Sales & Discount <br />Codes</h1>
+                    <p className="text-sm text-gray-500 mt-1 pt-2">by fashe-theme Admin on Dec 28, 2017</p>
+                    <p className="text-sm text-gray-400 mt-2 pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Etiam sed turpis sed lorem dignissim vulputate nec <br />cursus ante. Nunc sit...</p>
+                </div>
+                <div className="w-full md:w-1/3">
+                    <img
+                        src={imageOure1}
+                        alt=""
+                        className="w-full h-60 object-cover transition-transform duration-300 hover:scale-105 hover:shadow-xl"
+                    />
+                    <h1 className="text-gray-700 text-lg font-semibold mt-4 hover:text-red-400">The White Sneakers Nearly Every Fashion <br />Girls Own</h1>
+                    <p className="text-sm text-gray-500 mt-1 pt-2">by fashe-theme Admin on Dec 28, 2017</p>
+                    <p className="text-sm text-gray-400 mt-2 pt-3">Duis ut velit gravida nibh bibendum commodo. Suspendisse <br />pellentesque mattis augue id euismod. <br />Interdum et...</p>
+                </div>
+            </div>
+            <div></div>
 
-//         </section>
+        </section>
 
-//     );
-// };
+    );
+};
 
-// export default Blog;
+export default Blog;
